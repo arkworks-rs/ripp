c1abeb436029fcc7bee0ceba60cc871f4bd99abf
Nirvan Tyagi <nirvan.tyagi@gmail.com>
2020-07-29 22:21:54 -0400

    Tests for Pedersen and AFGHO commitments

--- dh_commitments/src/pedersen/mod.rs
+++ dh_commitments/src/pedersen/mod.rs
@@ -32,3 +32,36 @@ impl<G: Group> DoublyHomomorphicCommitment for PedersenCommitment<G> {
         )
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use algebra::{
+        ed_on_bls12_381::EdwardsAffine as JubJub,
+        UniformRand,
+    };
+    use rand::{rngs::StdRng, SeedableRng};
+
+    type C = PedersenCommitment<JubJub>;
+    const TEST_SIZE: usize = 8;
+
+    #[test]
+    fn pedersen_test() {
+        let mut rng = StdRng::seed_from_u64(0u64);
+        let commit_keys = C::setup(&mut rng, TEST_SIZE).unwrap();
+        let mut message = Vec::new();
+        let mut wrong_message = Vec::new();
+        for _ in 0..TEST_SIZE {
+            message.push(<JubJub as Group>::ScalarField::rand(&mut rng));
+            wrong_message.push(<JubJub as Group>::ScalarField::rand(&mut rng));
+        }
+        let com = C::commit(&commit_keys, &message).unwrap();
+        assert!(C::verify(&commit_keys, &message, &com).unwrap());
+        assert!(!C::verify(&commit_keys, &wrong_message, &com).unwrap());
+        message.push(<JubJub as Group>::ScalarField::rand(&mut rng));
+        assert!(C::verify(&commit_keys, &message, &com).is_err());
+
+    }
+}
+
