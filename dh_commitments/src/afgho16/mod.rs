00b49b7678bf83453eacd032c14757bbaeb4db21
Nirvan Tyagi <nirvan.tyagi@gmail.com>
2020-07-29 22:21:54 -0400

    Tests for Pedersen and AFGHO commitments

--- dh_commitments/src/afgho16/mod.rs
+++ dh_commitments/src/afgho16/mod.rs
@@ -55,3 +55,53 @@ impl<P: PairingEngine> DoublyHomomorphicCommitment for AFGHOCommitmentG2<P>
         ))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use algebra::{
+        bls12_381::Bls12_381,
+        UniformRand,
+    };
+    use rand::{rngs::StdRng, SeedableRng};
+
+    type C1 = AFGHOCommitmentG1<Bls12_381>;
+    type C2 = AFGHOCommitmentG2<Bls12_381>;
+    const TEST_SIZE: usize = 8;
+
+    #[test]
+    fn afgho_g1_test() {
+        let mut rng = StdRng::seed_from_u64(0u64);
+        let commit_keys = C1::setup(&mut rng, TEST_SIZE).unwrap();
+        let mut message = Vec::new();
+        let mut wrong_message = Vec::new();
+        for _ in 0..TEST_SIZE {
+            message.push(<Bls12_381 as PairingEngine>::G1Projective::rand(&mut rng));
+            wrong_message.push(<Bls12_381 as PairingEngine>::G1Projective::rand(&mut rng));
+        }
+        let com = C1::commit(&commit_keys, &message).unwrap();
+        assert!(C1::verify(&commit_keys, &message, &com).unwrap());
+        assert!(!C1::verify(&commit_keys, &wrong_message, &com).unwrap());
+        message.push(<Bls12_381 as PairingEngine>::G1Projective::rand(&mut rng));
+        assert!(C1::verify(&commit_keys, &message, &com).is_err());
+    }
+
+    #[test]
+    fn afgho_g2_test() {
+        let mut rng = StdRng::seed_from_u64(0u64);
+        let commit_keys = C2::setup(&mut rng, TEST_SIZE).unwrap();
+        let mut message = Vec::new();
+        let mut wrong_message = Vec::new();
+        for _ in 0..TEST_SIZE {
+            message.push(<Bls12_381 as PairingEngine>::G2Projective::rand(&mut rng));
+            wrong_message.push(<Bls12_381 as PairingEngine>::G2Projective::rand(&mut rng));
+        }
+        let com = C2::commit(&commit_keys, &message).unwrap();
+        assert!(C2::verify(&commit_keys, &message, &com).unwrap());
+        assert!(!C2::verify(&commit_keys, &wrong_message, &com).unwrap());
+        message.push(<Bls12_381 as PairingEngine>::G2Projective::rand(&mut rng));
+        assert!(C2::verify(&commit_keys, &message, &com).is_err());
+    }
+}
+
+
