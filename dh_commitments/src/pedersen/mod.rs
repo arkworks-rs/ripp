55ad00dc17520f6e8bbeb4af3c03123ca596f892
Nirvan Tyagi <nirvan.tyagi@gmail.com>
2020-09-08 18:10:08 -0400

    More efficient inner products with VariableMSM and sharing Miller loop

--- dh_commitments/src/pedersen/mod.rs
+++ dh_commitments/src/pedersen/mod.rs
@@ -1,4 +1,6 @@
-use algebra::groups::Group;
+use algebra::{
+    curves::ProjectiveCurve,
+};
 use rand::Rng;
 use std::marker::PhantomData;
 
@@ -7,11 +9,11 @@ use crate::{random_generators, DoublyHomomorphicCommitment, Error};
 use inner_products::{InnerProduct, MultiexponentiationInnerProduct};
 
 #[derive(Clone)]
-pub struct PedersenCommitment<G: Group> {
+pub struct PedersenCommitment<G: ProjectiveCurve> {
     _group: PhantomData<G>,
 }
 
-impl<G: Group> DoublyHomomorphicCommitment for PedersenCommitment<G> {
+impl<G: ProjectiveCurve> DoublyHomomorphicCommitment for PedersenCommitment<G> {
     type Scalar = G::ScalarField;
     type Message = G::ScalarField;
     type Key = G;
@@ -29,7 +31,7 @@ impl<G: Group> DoublyHomomorphicCommitment for PedersenCommitment<G> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use algebra::{ed_on_bls12_381::EdwardsAffine as JubJub, UniformRand};
+    use algebra::{ed_on_bls12_381::EdwardsProjective as JubJub, UniformRand};
     use rand::{rngs::StdRng, SeedableRng};
 
     type C = PedersenCommitment<JubJub>;
@@ -42,13 +44,13 @@ mod tests {
         let mut message = Vec::new();
         let mut wrong_message = Vec::new();
         for _ in 0..TEST_SIZE {
-            message.push(<JubJub as Group>::ScalarField::rand(&mut rng));
-            wrong_message.push(<JubJub as Group>::ScalarField::rand(&mut rng));
+            message.push(<JubJub as ProjectiveCurve>::ScalarField::rand(&mut rng));
+            wrong_message.push(<JubJub as ProjectiveCurve>::ScalarField::rand(&mut rng));
         }
         let com = C::commit(&commit_keys, &message).unwrap();
         assert!(C::verify(&commit_keys, &message, &com).unwrap());
         assert!(!C::verify(&commit_keys, &wrong_message, &com).unwrap());
-        message.push(<JubJub as Group>::ScalarField::rand(&mut rng));
+        message.push(<JubJub as ProjectiveCurve>::ScalarField::rand(&mut rng));
         assert!(C::verify(&commit_keys, &message, &com).is_err());
     }
 }
