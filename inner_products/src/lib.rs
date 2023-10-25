af9405e9dcdf49d31a3757689747b8e35d16deab
Michael Rosenberg <michael@mrosenberg.pub>
2023-10-25 15:11:21 -0400

    Upgraded ip_proofs to arkworks 0.4

--- inner_products/src/lib.rs
+++ inner_products/src/lib.rs
@@ -1,13 +1,13 @@
-use ark_ec::{msm::VariableBaseMSM, PairingEngine, ProjectiveCurve};
-use ark_ff::{bytes::ToBytes, Field, PrimeField};
-use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Read, SerializationError};
-use ark_std::{cfg_into_iter, cfg_iter};
+use ark_ec::{
+    pairing::{Pairing, PairingOutput},
+    CurveGroup,
+};
+use ark_ff::Field;
+use ark_std::cfg_iter;
 use std::{
     error::Error as ErrorTrait,
     fmt::{Display, Formatter, Result as FmtResult},
-    io::{Result as IoResult, Write},
     marker::PhantomData,
-    ops::{Add, Mul, MulAssign},
 };
 
 #[cfg(feature = "parallel")]
@@ -49,14 +49,14 @@ pub trait InnerProduct: Copy {
 }
 
 #[derive(Copy, Clone)]
-pub struct PairingInnerProduct<P: PairingEngine> {
+pub struct PairingInnerProduct<P: Pairing> {
     _pair: PhantomData<P>,
 }
 
-impl<P: PairingEngine> InnerProduct for PairingInnerProduct<P> {
-    type LeftMessage = P::G1Projective;
-    type RightMessage = P::G2Projective;
-    type Output = ExtensionFieldElement<P>;
+impl<P: Pairing> InnerProduct for PairingInnerProduct<P> {
+    type LeftMessage = P::G1;
+    type RightMessage = P::G2;
+    type Output = PairingOutput<P>;
 
     fn inner_product(
         left: &[Self::LeftMessage],
@@ -68,22 +68,18 @@ impl<P: PairingEngine> InnerProduct for PairingInnerProduct<P> {
                 right.len(),
             )));
         };
-        let aff_left = P::G1Projective::batch_normalization_into_affine(left);
-        let aff_right = P::G2Projective::batch_normalization_into_affine(right);
-        let aff_pairs = cfg_into_iter!(aff_left)
-            .zip(aff_right)
-            .map(|(a, b)| (P::G1Prepared::from(a), P::G2Prepared::from(b)))
-            .collect::<Vec<_>>();
-        Ok(ExtensionFieldElement(P::product_of_pairings(&aff_pairs)))
+        let aff_left = P::G1::normalize_batch(left);
+        let aff_right = P::G2::normalize_batch(right);
+        Ok(P::multi_pairing(&aff_left, &aff_right))
     }
 }
 
 #[derive(Copy, Clone)]
-pub struct MultiexponentiationInnerProduct<G: ProjectiveCurve> {
+pub struct MultiexponentiationInnerProduct<G: CurveGroup> {
     _projective: PhantomData<G>,
 }
 
-impl<G: ProjectiveCurve> InnerProduct for MultiexponentiationInnerProduct<G> {
+impl<G: CurveGroup> InnerProduct for MultiexponentiationInnerProduct<G> {
     type LeftMessage = G;
     type RightMessage = G::ScalarField;
     type Output = G;
@@ -98,11 +94,9 @@ impl<G: ProjectiveCurve> InnerProduct for MultiexponentiationInnerProduct<G> {
                 right.len(),
             )));
         };
-        let right_bigints = cfg_iter!(right).map(|b| b.into_repr()).collect::<Vec<_>>();
-        Ok(VariableBaseMSM::multi_scalar_mul(
-            &G::batch_normalization_into_affine(left),
-            &right_bigints,
-        ))
+
+        // Can unwrap because we did the length check above
+        Ok(G::msm(&G::normalize_batch(left), &right).unwrap())
     }
 }
 
@@ -127,45 +121,5 @@ impl<F: Field> InnerProduct for ScalarInnerProduct<F> {
             )));
         };
         Ok(cfg_iter!(left).zip(right).map(|(x, y)| *x * y).sum())
-    }
-}
-
-// Helper wrapper type around target group commitment output in order to implement MulAssign (needed for dh_commitments)
-//TODO: PairingEngine provides target group GT implementing Group for prime order P::Fr
-
-#[derive(CanonicalSerialize, CanonicalDeserialize, Clone, Debug)]
-pub struct ExtensionFieldElement<P: PairingEngine>(pub P::Fqk);
-
-impl<P: PairingEngine> Default for ExtensionFieldElement<P> {
-    fn default() -> Self {
-        ExtensionFieldElement(<P::Fqk>::default())
-    }
-}
-
-impl<P: PairingEngine> PartialEq for ExtensionFieldElement<P> {
-    fn eq(&self, other: &Self) -> bool {
-        self.0.eq(&other.0)
-    }
-}
-
-impl<P: PairingEngine> Eq for ExtensionFieldElement<P> {}
-
-impl<P: PairingEngine> MulAssign<P::Fr> for ExtensionFieldElement<P> {
-    fn mul_assign(&mut self, rhs: P::Fr) {
-        *self = ExtensionFieldElement(self.0.pow(rhs.into_repr()))
-    }
-}
-
-impl<P: PairingEngine> Add<Self> for ExtensionFieldElement<P> {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        ExtensionFieldElement(<P::Fqk as Mul>::mul(self.0, rhs.0))
-    }
-}
-
-impl<P: PairingEngine> ToBytes for ExtensionFieldElement<P> {
-    fn write<W: Write>(&self, mut writer: W) -> IoResult<()> {
-        self.0.write(&mut writer)
     }
 }
